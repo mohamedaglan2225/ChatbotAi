8c2120a8e1f261ab8a294c88afe03bcb884df96f
Mohamed Aglan <mohamedaglan.dev@gmail.com>
2024-05-11 12:25:14 +0300

    Handle Create Room

--- Sources/ChatbotAi/ChatFramework/Controller/ChatView/ChatView.swift
+++ Sources/ChatbotAi/ChatFramework/Controller/ChatView/ChatView.swift
@@ -120,7 +120,7 @@ public class ChatView: UIView {
     
     
     private func fetchCoreDataMessages() {
-        let room = storage.getOrCreateRoom(with: roomName, forceNew: false)
+        let room = storage.getOrCreateRoom(with: roomName, forceNew: true)
         self.roomId = Int(room.roomId)
         self.chatModel = self.storage.fetchMessages(roomId: roomId)
         tableView.reloadData()
