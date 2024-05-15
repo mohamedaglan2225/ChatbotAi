0cc967c70b20c24df9129ce3f3790051f61222ba
Mohamed Aglan <mohamedaglan.dev@gmail.com>
2024-05-15 17:55:23 +0300

    Handle Create Room

--- Sources/ChatbotAi/ChatFramework/Controller/ChatView/MessagesView.swift
+++ Sources/ChatbotAi/ChatFramework/Controller/ChatView/MessagesView.swift
@@ -16,5 +16,14 @@ class MessagesView: UIViewController {
         
         
     }
+    
+    
+    public init() {
+        super.init(nibName: "MessagesView", bundle: Bundle.module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
